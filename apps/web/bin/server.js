c985bea8ea71801af09ddf00ff003e3e4283429c
Mitsuki Ogasahara <negiga@gmail.com>
2024-12-13 17:36:10 +0900

    feat: Update to be installed and run @synk-cal/web

---
+++ apps/web/bin/server.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { createRequestHandler } from '@remix-run/express'
+import express from 'express'
+import path from 'node:path'
+
+const __dirname = new URL('.', import.meta.url).pathname
+const buildDir = path.join(__dirname, '..', 'build')
+
+const app = express()
+app.use(express.static(path.join(buildDir, 'client')))
+const build = await import(path.join(buildDir, 'server', 'index.js'))
+
+app.all('*', createRequestHandler({ build }))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`App listening on http://localhost:${PORT}`)
+})
